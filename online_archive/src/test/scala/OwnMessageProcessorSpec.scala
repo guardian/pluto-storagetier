c3d8dfa42e6f95097796b655937e017556da540d
andy_gallagher <andy.gallagher@theguardian.com>
2021-09-24 19:51:00 +0100

    tests for OwnMessageProcessor

--- online_archive/src/test/scala/OwnMessageProcessorSpec.scala
+++ online_archive/src/test/scala/OwnMessageProcessorSpec.scala
@@ -6,9 +6,11 @@ import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import io.circe.generic.auto._
 import io.circe.syntax._
+
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Await, Future}
+import scala.util.Try
 
 class OwnMessageProcessorSpec extends Specification with Mockito {
   "OwnMessageProcessor.handleArchivehunterValidation" should {
@@ -47,6 +49,102 @@ class OwnMessageProcessorSpec extends Specification with Mockito {
       there was one(archiveHunterCommunicator).lookupArchivehunterId("abcdefg","testbucket","path/to/some.file")
       there was one(archivedRecordDAO).updateIdValidationStatus(1234, true)
       there was one(archivedRecordDAO).getRecord(1234)
+    }
+
+    "return a Left and not update the record if the ID is not valid" in {
+      implicit val mat:Materializer = mock[Materializer]
+      implicit val system:ActorSystem = mock[ActorSystem]
+      implicit val archivedRecordDAO = mock[ArchivedRecordDAO]
+      implicit val archiveHunterCommunicator = mock[ArchiveHunterCommunicator]
+
+      archiveHunterCommunicator.lookupArchivehunterId(any,any,any) returns Future(false)
+      val updatedRecord = mock[ArchivedRecord]
+      archivedRecordDAO.getRecord(any) returns Future(Some(updatedRecord))
+      archivedRecordDAO.updateIdValidationStatus(any,any) returns Future(1)
+
+      val toTest = new OwnMessageProcessor(ArchiveHunterConfig("https://some.base","secret-goes-here"))
+
+      val incomingRecord = ArchivedRecord(
+        Some(1234),
+        "abcdefg",
+        false,
+        "/from/root/path/to/some.file",
+        "testbucket",
+        "path/to/some.file",
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None
+      )
+      val result = Await.result(toTest.handleArchivehunterValidation(incomingRecord.asJson), 3.seconds)
+
+      result must beLeft
+      there was one(archiveHunterCommunicator).lookupArchivehunterId("abcdefg","testbucket","path/to/some.file")
+      there was no(archivedRecordDAO).updateIdValidationStatus(any,any)
+      there was no(archivedRecordDAO).getRecord(any)
+    }
+
+    "pass on the failure if the lookup crashes for some reason" in {
+      implicit val mat:Materializer = mock[Materializer]
+      implicit val system:ActorSystem = mock[ActorSystem]
+      implicit val archivedRecordDAO = mock[ArchivedRecordDAO]
+      implicit val archiveHunterCommunicator = mock[ArchiveHunterCommunicator]
+
+      archiveHunterCommunicator.lookupArchivehunterId(any,any,any) returns Future.failed(new RuntimeException("kaboom"))
+      val updatedRecord = mock[ArchivedRecord]
+      archivedRecordDAO.getRecord(any) returns Future(Some(updatedRecord))
+      archivedRecordDAO.updateIdValidationStatus(any,any) returns Future(1)
+
+      val toTest = new OwnMessageProcessor(ArchiveHunterConfig("https://some.base","secret-goes-here"))
+
+      val incomingRecord = ArchivedRecord(
+        Some(1234),
+        "abcdefg",
+        false,
+        "/from/root/path/to/some.file",
+        "testbucket",
+        "path/to/some.file",
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None
+      )
+      val result = Try { Await.result(toTest.handleArchivehunterValidation(incomingRecord.asJson), 3.seconds) }
+
+      result must beFailedTry
+      there was one(archiveHunterCommunicator).lookupArchivehunterId("abcdefg","testbucket","path/to/some.file")
+      there was no(archivedRecordDAO).updateIdValidationStatus(any,any)
+      there was no(archivedRecordDAO).getRecord(any)
+    }
+
+    "return a failure if the given message is not in the right format" in {
+      implicit val mat:Materializer = mock[Materializer]
+      implicit val system:ActorSystem = mock[ActorSystem]
+      implicit val archivedRecordDAO = mock[ArchivedRecordDAO]
+      implicit val archiveHunterCommunicator = mock[ArchiveHunterCommunicator]
+
+      archiveHunterCommunicator.lookupArchivehunterId(any,any,any) returns Future.failed(new RuntimeException("kaboom"))
+      val updatedRecord = mock[ArchivedRecord]
+      archivedRecordDAO.getRecord(any) returns Future(Some(updatedRecord))
+      archivedRecordDAO.updateIdValidationStatus(any,any) returns Future(1)
+
+      val toTest = new OwnMessageProcessor(ArchiveHunterConfig("https://some.base","secret-goes-here"))
+
+      val incomingRecord = Map[String,String]("key"->"value")
+      val result = Try { Await.result(toTest.handleArchivehunterValidation(incomingRecord.asJson), 3.seconds) }
+
+      result must beFailedTry
+      there was no(archiveHunterCommunicator).lookupArchivehunterId(any,any,any)
+      there was no(archivedRecordDAO).updateIdValidationStatus(any,any)
+      there was no(archivedRecordDAO).getRecord(any)
     }
   }
 }
