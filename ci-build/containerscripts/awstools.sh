970645eeeb43623c56908d983f45ec65934fca87
andy_gallagher <andy.gallagher@theguardian.com>
2021-09-28 12:27:59 +0100

    fix aws command installation and add metadata output

--- ci-build/containerscripts/awstools.sh
+++ ci-build/containerscripts/awstools.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get -y install python3
-curl "https://bootstrap.pypa.io/get-pip.py" -o "/tmp/get-pip.py"
-chmod a+x /tmp/get-pip.py
-python3 /tmp/get-pip.py
-rm -f /tmp/get-pip.py
+apt-get -y install python3 python3-pip
 
 pip3 install awscli
 
