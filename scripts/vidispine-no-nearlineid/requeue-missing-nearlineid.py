ec9d6f83a6e0ea016e3a63a253635724f24b0bad
andy_gallagher <andy.gallagher@theguardian.com>
2022-03-21 15:09:50 +0000

    bugfixes

--- scripts/vidispine-no-nearlineid/requeue-missing-nearlineid.py
+++ scripts/vidispine-no-nearlineid/requeue-missing-nearlineid.py
@@ -36,12 +36,20 @@ def load_vs_credentials(basepath:str) -> (str, str):
     :return: a tuple of (username, password). Raises an exception on error
     """
     logger.info("Reading vidispine credentials from {0}".format(basepath))
-    with open(join(basepath, "vidispine_admin_user")) as f:
-        username = f.read()
-    with open(join(basepath, "vidispine_admin_password")) as f:
-        pwd = f.read()
+    try:
+        with open(join(basepath, "vsuser")) as f:
+            username = f.read()
+        with open(join(basepath, "vspass")) as f:
+            pwd = f.read()
 
-    return username, pwd
+        return username, pwd
+    except FileNotFoundError:
+        with open(join(basepath, "vidispine_admin_user")) as f:
+            username = f.read()
+        with open(join(basepath, "vidispine_admin_password")) as f:
+            pwd = f.read()
+
+        return username, pwd
 
 
 class RMQCredentials(object):
@@ -96,6 +104,7 @@ parser.add_argument("--rmquser", default="admin", help="RabbitMQ user, as an alt
 parser.add_argument("--rmqpasswd", default="admin", help="RabbitMQ password, as an alternative to using rmq-credentials")
 parser.add_argument("--vhost", default="pluto-ng", help="RabbitMQ VHhost")
 parser.add_argument("--limit", default=None, help="Only process up to this many items")
+parser.add_argument("--count", default=False, action="store_true", help="Don't add anything to a queue, just count the number of items")
 args = parser.parse_args()
 
 if not args.rmquser or not args.rmqpasswd:
@@ -127,12 +136,17 @@ search = VSSearch(host=args.vshost, port=args.vsport, https=args.https, user=vsc
 search.addCriterion({"gnm_nearline_id": ""})
 action = search.execute()
 
-logger.info("Found {0} items without gnm_nearline_id", action.totalItems)
+logger.info("Found {0} items without gnm_nearline_id".format(action.totalItems))
+if args.count:
+    sys.exit(0)
+
 ctr = 0
 for item in action.results(shouldPopulate=False):
     ctr += 1
     logger.info("Sending event {0}/{1} for {2}....".format(ctr, action.totalItems, item.name))
     send_message(rmq_chan, item.name)
-    if args.limit is not None and int(args.limit)>=ctr:
+    if args.limit is not None and ctr>=int(args.limit):
         logger.info("Hit limit of {0} items, completing".format(args.limit))
+        break
+
 logger.info("All done.")
