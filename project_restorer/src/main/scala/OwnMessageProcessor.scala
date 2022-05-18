1155efeb049fc7fc98ab2259fe9daee4c177e059
Daniel Hedlund <daniel.hedlund@codemill.se>
2022-05-18 13:30:26 +0200

    Add first test

--- project_restorer/src/main/scala/OwnMessageProcessor.scala
+++ project_restorer/src/main/scala/OwnMessageProcessor.scala
@@ -1,9 +1,15 @@
 import com.gu.multimedia.storagetier.framework.{MessageProcessor, MessageProcessorReturnValue}
+import com.gu.multimedia.storagetier.models.online_archive.ArchivedRecord
 import io.circe.Json
+import io.circe.generic.auto.exportDecoder
 
 import scala.concurrent.Future
+import org.slf4j.LoggerFactory
+
 
 class OwnMessageProcessor extends MessageProcessor {
+  private val logger = LoggerFactory.getLogger(getClass)
+
   /**
    * Override this method in your subclass to handle an incoming message
    *
@@ -14,5 +20,18 @@ class OwnMessageProcessor extends MessageProcessor {
    *         with a circe Json body (can be done with caseClassInstance.noSpaces) containing a message body to send
    *         to our exchange with details of the completed operation
    */
-  override def handleMessage(routingKey: String, msg: Json): Future[Either[String, MessageProcessorReturnValue]] = ???
+  override def handleMessage(routingKey: String, msg: Json): Future[Either[String, MessageProcessorReturnValue]] ={
+    routingKey match {
+      case "core.project.update"=>
+        msg.as[ArchivedRecord] match {
+          case Left(err)=>
+            Future.failed(new RuntimeException(s"Could not unmarshal json message ${msg.noSpaces} into an moop: $err"))
+          case Right(msg)=>
+            logger.info("Hello Mr Right")
+        }
+      case _=>
+        logger.warn(s"Dropping message $routingKey from own exchange as I don't know how to handle it. This should be fixed in the code.")
+        Future.failed(new RuntimeException("Not meant to receive this"))
+    }
+  }
 }
---
+++ project_restorer/src/test/scala/OwnMessageProcessorTest.scala
@@ -0,0 +1,10 @@
+import org.specs2.mutable.Specification
+
+class OwnMessageProcessorTest extends Specification {
+
+  "OwnMessageProcessorTest" should {
+    "handleMessage" in {
+      ok
+    }
+  }
+}
