8967e0dc74ae4921aa7500711eef229f988fa76f
Joel Sandberg <joel.sandberg@codemill.se>
2022-05-18 13:14:20 +0200

    Extend class

--- project_restorer/src/main/scala/OwnMessageProcessor.scala
+++ project_restorer/src/main/scala/OwnMessageProcessor.scala
@@ -1,3 +1,18 @@
-class OwnMessageProcessor {
+import com.gu.multimedia.storagetier.framework.{MessageProcessor, MessageProcessorReturnValue}
+import io.circe.Json
 
+import scala.concurrent.Future
+
+class OwnMessageProcessor extends MessageProcessor {
+  /**
+   * Override this method in your subclass to handle an incoming message
+   *
+   * @param routingKey the routing key of the message as received from the broker.
+   * @param msg        the message body, as a circe Json object. You can unmarshal this into a case class by
+   *                   using msg.as[CaseClassFormat]
+   * @return You need to return Left() with a descriptive error string if the message could not be processed, or Right
+   *         with a circe Json body (can be done with caseClassInstance.noSpaces) containing a message body to send
+   *         to our exchange with details of the completed operation
+   */
+  override def handleMessage(routingKey: String, msg: Json): Future[Either[String, MessageProcessorReturnValue]] = ???
 }
