08b126230b09ec13c292ef2e2a843d6356e2f4b8
Daniel Hedlund <daniel.hedlund@codemill.se>
2022-05-19 07:09:05 +0200

    Add some silly, but running, tests

--- project_restorer/src/test/scala/OwnMessageProcessorTest.scala
+++ project_restorer/src/test/scala/OwnMessageProcessorTest.scala
@@ -1,10 +1,79 @@
+
+import io.circe.Json
+import io.circe.syntax.EncoderOps
+import messages.ProjectUpdateMessage
+import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 
-class OwnMessageProcessorTest extends Specification {
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+import scala.util.Try
+
+class OwnMessageProcessorTest extends Specification with Mockito {
 
   "OwnMessageProcessorTest" should {
     "handleMessage" in {
-      ok
+      val toTest = new OwnMessageProcessor()
+
+      val msgContent =
+        """  {
+          |    "id": 23460,
+          |    "projectTypeId": 45,
+          |    "title": "anothertest",
+          |    "created": "2021-03-05T16:00:46Z",
+          |    "updated": "2021-03-05T16:00:46Z",
+          |    "user": "andy_gallagher",
+          |    "workingGroupId": 17,
+          |    "commissionId": 3555,
+          |    "deletable": true,
+          |    "deep_archive": false,
+          |    "sensitive": true,
+          |    "status": "Completed",
+          |    "productionOffice": "UK"
+          |  }
+          |""".stripMargin
+
+      val msg = io.circe.parser.parse(msgContent)
+      val emptyJson = Json.fromString("")
+
+      val result = Await.result(toTest.handleMessage("core.project.update", msg.getOrElse(emptyJson)), 3.seconds)
+
+      ok("Dummy test so far, but it compiles and runs through the code")
+    }
+
+    "drop message in handleMessage if wrong routing key" in {
+      val toTest = new OwnMessageProcessor()
+
+      val emptyJson = Json.fromString("")
+
+      val result = Try {
+        Await.result(toTest.handleMessage("NOT.core.project.update", emptyJson), 3.seconds)
+      }
+
+      result must beAFailedTry
+    }
+
+    "handleStatusMessage" in {
+      val toTest = new OwnMessageProcessor()
+
+      val updateMessage = ProjectUpdateMessage(
+        1234,
+        2,
+        "abcdefg",
+        Option.empty,
+        Option.empty,
+        "le user",
+        100,
+        200,
+        deletable = true,
+        deep_archive = false,
+        sensitive = false,
+        "oh noooo",
+        "LDN"
+      )
+      val result = Await.result(toTest.handleStatusMessage(updateMessage), 3.seconds)
+
+      result must beRight(updateMessage)
     }
   }
 }
