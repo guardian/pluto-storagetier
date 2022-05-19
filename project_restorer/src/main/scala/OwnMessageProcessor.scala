8e7a2bf43270ba0f4458803caa6e993f864f52d6
Daniel Hedlund <daniel.hedlund@codemill.se>
2022-05-19 07:08:25 +0200

    Refactor to make more testable

--- project_restorer/src/main/scala/OwnMessageProcessor.scala
+++ project_restorer/src/main/scala/OwnMessageProcessor.scala
@@ -1,25 +1,21 @@
+import com.gu.multimedia.storagetier.framework.MessageProcessorConverters._
 import com.gu.multimedia.storagetier.framework.{MessageProcessor, MessageProcessorReturnValue}
-import com.gu.multimedia.storagetier.models.online_archive.ArchivedRecord
 import io.circe.Json
-import io.circe.generic.auto.exportDecoder
+import io.circe.generic.auto.{exportDecoder, exportEncoder}
+import io.circe.syntax.EncoderOps
 import messages.ProjectUpdateMessage
-
-import scala.concurrent.Future
 import org.slf4j.LoggerFactory
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 
 class OwnMessageProcessor extends MessageProcessor {
   private val logger = LoggerFactory.getLogger(getClass)
 
-  def handleStatusMessage(msg: Json) : Future[Either[String, MessageProcessorReturnValue]] = {
-    msg.as[ProjectUpdateMessage] match {
-      case Left(err)=>
-        Future.failed(new RuntimeException(s"Could not unmarshal json message ${msg.noSpaces} into an ProjectUpdate: $err"))
-
-      case Right(updateMessage)=>
-        logger.info(s"here is an update status ${updateMessage.status}")
-
-    }
+  def handleStatusMessage(updateMessage: ProjectUpdateMessage): Future[Either[String, MessageProcessorReturnValue]] = {
+    logger.info(s"here is an update status ${updateMessage.status}")
+    Future(Right(updateMessage.asJson))
   }
 
   /**
@@ -32,17 +28,18 @@ class OwnMessageProcessor extends MessageProcessor {
    *         with a circe Json body (can be done with caseClassInstance.noSpaces) containing a message body to send
    *         to our exchange with details of the completed operation
    */
-  override def handleMessage(routingKey: String, msg: Json): Future[Either[String, MessageProcessorReturnValue]] ={
+  override def handleMessage(routingKey: String, msg: Json): Future[Either[String, MessageProcessorReturnValue]] = {
+    logger.info(s"Received message of $routingKey from queue: ${msg.noSpaces}")
     routingKey match {
-      case "core.project.update"=>
-        msg.as[Json] match {
-          case Left(err)=>
-            Future.failed(new RuntimeException(s"Could not unmarshal json message ${msg.noSpaces} into an moop: $err"))
-          case Right(msg)=>
-            handleStatusMessage(msg)
-
+      case "core.project.update" =>
+        msg.as[ProjectUpdateMessage] match {
+          case Left(err) =>
+            Future.failed(new RuntimeException(s"Could not unmarshal json message ${msg.noSpaces} into an ProjectUpdate: $err"))
+          case Right(updateMessage) =>
+            logger.info(s"here is an update status ${updateMessage.status}")
+            handleStatusMessage(updateMessage)
         }
-      case _=>
+      case _ =>
         logger.warn(s"Dropping message $routingKey from own exchange as I don't know how to handle it. This should be fixed in the code.")
         Future.failed(new RuntimeException("Not meant to receive this"))
     }
