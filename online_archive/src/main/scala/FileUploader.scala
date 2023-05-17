c139093c2969b497686cbea972c99825a19a1059
Richard Pryce <richard.pryce@guardian.co.uk>
2023-05-17 14:55:44 +0100

    Set minimum part size to 20MB

--- online_archive/src/main/scala/FileUploader.scala
+++ online_archive/src/main/scala/FileUploader.scala
@@ -186,8 +186,6 @@ class FileUploader(transferManager: S3TransferManager, client: S3Client, var buc
     import scala.jdk.FutureConverters._
     val loggerContext = Option(MDC.getCopyOfContextMap)
 
-
-
     val basePutReq = PutObjectRequest.builder()
       .bucket(bucketName)
       .key(keyName)
@@ -234,7 +232,7 @@ class FileUploader(transferManager: S3TransferManager, client: S3Client, var buc
 
 object FileUploader {
   private def s3ClientConfig = {
-    val b = S3CrtAsyncClient.builder()
+    val b = S3CrtAsyncClient.builder().minimumPartSizeInBytes(20 * 1024)
     val withRegion = sys.env.get("AWS_REGION") match {
       case Some(rgn)=>b.region(Region.of(rgn))
       case None=>b
